22a588c7cecf10a449e1d680dce3b056cb061a81
heyimmatt <mrmcfarland@gmail.com>
2020-09-04 12:24:05 -0700

    Add query logic to show warbles from followed users instead of all users

--- app.py
+++ app.py
@@ -318,8 +318,10 @@ def homepage():
     """
 
     if g.user:
+        following_ids = [user.id for user in g.user.following]
         messages = (Message
                     .query
+                    .filter(Message.user_id.in_(following_ids))
                     .order_by(Message.timestamp.desc())
                     .limit(100)
                     .all())
